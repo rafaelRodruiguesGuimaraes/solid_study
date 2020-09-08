18ea3dd07c3acb76eb682c748f56767c30117008
Rafael Guimarães <rafarodgui@hotmail.com>
2020-09-08 17:50:54 -0300

    Single Responsibility Principle and Liskov Substitution Principle finished

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,5 +1,6 @@
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
+import { User } from '../../entities/Users';
 import { ICreateUserRequestDTO } from './CreateUserDTO';
 
 export class CreateUserUseCase {
@@ -9,11 +10,15 @@ export class CreateUserUseCase {
 
     }
 
-    async execute ({ email }: ICreateUserRequestDTO) {
-        const userAlreadyExists = await this.usersRepository.findByEmail(email)
+    async execute (data: ICreateUserRequestDTO) {
+        const userAlreadyExists = await this.usersRepository.findByEmail(data.email)
 
         if(userAlreadyExists) {
             throw new Error('This e-mail is already in use')
         }
+
+        const user = new User(data);
+
+        await this.usersRepository.save(user);
     }
 }
